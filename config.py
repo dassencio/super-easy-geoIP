036a494491f4a698c4b0889c0d80f4d48d729fa5
Diego Assencio <diego@assencio.com>
2015-11-27 16:20:26 +0100

    fixes documentation

--- config.py
+++ config.py
@@ -1,25 +1,25 @@
 ################################################################################
 #
-#  IMPORTANT: if you change the numbers below, regenerate the database
+#  IMPORTANT: regenerate the database if you change the numbers below,
 #             otherwise the querying script will stop working properly!
 #
 ################################################################################
 
 #
-# Sizes of IPv4/6 netmask blocks. Increasing these numbers will increase the
-# speed of the querying database (netmask -> geoname ID) segment files (since
+# Sizes of IPv4/6 netmask blocks. Decreasing these numbers will increase the
+# querying speed for database (netmask -> geoname ID) segment files (since
 # there will be more of them and they will be therefore smaller) at the cost of
-# decreasing the speed of querying the associated index file (since it will be
+# decreasing the querying speed for the associated index file (since it will be
 # bigger: more segments => longer index).
 #
 ip4_block = 1000
 ip6_block = 1000
 
 #
-# Size of geoname ID blocks. Increasing this number will increase the speed of
-# querying the database (geoname ID -> geolocation data) segment files (since
+# Size of geoname ID blocks. Decreasing this number will increase the querying
+# speed for the database (geoname ID -> geolocation data) segment files (since
 # there will be more of them and they will be therefore smaller) at the cost
-# of decreasing the speed of querying the associated index file (since it
+# of decreasing the querying speed for the associated index file (since it
 # will be bigger: more segments => longer index).
 #
 geoid_block = 500
