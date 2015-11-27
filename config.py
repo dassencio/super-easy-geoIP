6e8388bc3b7e568320366cc8a3aee7c78d95025c
Diego Assencio <diego@assencio.com>
2015-11-27 10:50:15 +0100

    a few minor documentation fixes

--- config.py
+++ config.py
@@ -17,9 +17,9 @@ ip6_block = 1000
 
 #
 # Size of geoname ID blocks. Increasing this number will increase the speed of
-# querying the database (geoname ID -> geographical information) segment files
-# (since there will be more of them and they will be therefore smaller) at the
-# cost of decreasing the speed of querying the associated index file (since it
+# querying the database (geoname ID -> geolocation data) segment files (since
+# there will be more of them and they will be therefore smaller) at the cost
+# of decreasing the speed of querying the associated index file (since it
 # will be bigger: more segments => longer index).
 #
 geoid_block = 500
