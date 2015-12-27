998a0c740c98ed2a22135ce3e705e278e95b3fc1
Diego Assencio <diego@assencio.com>
2015-12-27 10:56:05 +0100

    improves variable and function names

--- ipfunctions.py
+++ ipfunctions.py
@@ -6,37 +6,41 @@ import socket
 import binascii
 
 
-def netmask_to_ip_range(netmask_cidr):
+def subnetwork_to_ip_range(subnetwork_cidr):
 
 	"""
-	Returns a tuple (ip_lower, ip_upper) containing the integer
-	representation of the lower and upper IP address values in the
-	range specified by a given netmask string in CIDR notation.
+	Returns a tuple (ip_lower, ip_upper) containing the integer values
+	of the lower and upper IP addresses respectively in a subnetwork
+	expressed in CIDR notation (as a string).
 
-	Both IPv4 netmasks (e.g. "192.168.1.1/24") and IPv6 netmasks
+	Both IPv4 subnetworks (e.g. "192.168.1.1/24") and IPv6 subnetworks
 	(e.g. "2a02:a448:ddb0::/44") are accepted.
 	"""
 
 	try:
-		fragments = netmask_cidr.split('/')
-		ip_address = fragments[0]
+		fragments = subnetwork_cidr.split('/')
+		network_prefix = fragments[0]
 		netmask_len = int(fragments[1])
 
-		for (max_len, version) in ((32,socket.AF_INET), (128,socket.AF_INET6)):
+		# try parsing the subnetwork first as IPv4, then IPv6
+		for version in (socket.AF_INET, socket.AF_INET6):
+
+			ip_len = 32 if version == socket.AF_INET else 128
 
 			try:
-				suffix_mask = (1 << (max_len - netmask_len)) - 1
-				netmask = ((1 << max_len) - 1) - suffix_mask
-				ip_hex = socket.inet_pton(version, ip_address)
+				suffix_mask = (1 << (ip_len - netmask_len)) - 1
+				netmask = ((1 << ip_len) - 1) - suffix_mask
+				ip_hex = socket.inet_pton(version, network_prefix)
 				ip_lower = int(binascii.hexlify(ip_hex), 16) & netmask
+				ip_upper = ip_lower + suffix_mask
 
-				return (ip_lower, ip_lower + suffix_mask)
+				return (ip_lower, ip_upper)
 			except:
 				pass
 	except:
 		pass
 
-	raise ValueError("invalid netmask")
+	raise ValueError("invalid subnetwork")
 
 
 def ip_to_integer(ip_address):
@@ -47,15 +51,15 @@ def ip_to_integer(ip_address):
 	either 4 or 6.
 
 	Both IPv4 addresses (e.g. "192.168.1.1") and IPv6 addresses
-	(e.g. "2a02:a448:ddb0::") are accepted
+	(e.g. "2a02:a448:ddb0::") are accepted.
 	"""
 
 	# try parsing the IP address first as IPv4, then IPv6
 	for version in (socket.AF_INET, socket.AF_INET6):
 
 		try:
-			ip_hexstr = socket.inet_pton(version, ip_address)
-			ip_integer = int(binascii.hexlify(ip_hexstr), 16)
+			ip_hex = socket.inet_pton(version, ip_address)
+			ip_integer = int(binascii.hexlify(ip_hex), 16)
 
 			return (ip_integer, 4 if version == socket.AF_INET else 6)
 		except:
